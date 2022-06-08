c0d36f439d06a52466234d11c5f67d8063c16548
Yash Togadiya <yashtogadiya3@gmail.com>
2022-06-08 12:17:20 +0530

    Mini player and Video Player Closing icon.

--- src/audio/audiodetails.js
+++ src/audio/audiodetails.js
@@ -40,6 +40,9 @@ function miniPlayer(id) {
             <h3> ` + audioDetails[id].artist + `</h3>
         </div>
         <audio src="` + audioDetails[id].src + `" controls autoplay></audio>
+        <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-x" id="closeMiniPlayer" onclick="closeMiniPlayer()" viewBox="0 0 16 16">
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+        </svg>
     `;
     document.getElementById("miniPlayer").appendChild(miniPlayer)
 }
@@ -48,4 +51,8 @@ function playAudio(id) {
     document.getElementById("miniPlayer").innerHTML = null;
     document.getElementById("videoPlayer").innerHTML = null;
     miniPlayer(id);
+}
+
+function closeMiniPlayer() {
+    document.getElementById("miniPlayer").innerHTML = null;
 }
--- src/video/videoDetails.js
+++ src/video/videoDetails.js
@@ -34,14 +34,23 @@ const videoDetails = [
 function videoPlayer(id) {
     const videoPlayer = document.getElementById("videoPlayer");
 
-    videoPlayer.innerHTML =
-        `<video width="500" height="300" controls autoplay>
+    videoPlayer.innerHTML = `
+        <video width="500" height="300" controls autoplay>
             <source src="`+ videoDetails[id].src + `" type="video/mp4">
-        </video>`;
+        </video>
+        <svg xmlns="http://www.w3.org/2000/svg" style="color: white;" fill="currentColor" class="bi bi-x" id="closeVideoPlayer" onclick="closeVideoPlayer()" viewBox="0 0 16 16">
+            <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+        </svg>
+    `;
 }
 
 function playVideo(id) {
     document.getElementById("videoPlayer").innerHTML = null;
     document.getElementById("miniPlayer").innerHTML = null;
     videoPlayer(id);
+}
+
+
+function closeVideoPlayer() {
+    document.getElementById("videoPlayer").innerHTML = null;
 }
