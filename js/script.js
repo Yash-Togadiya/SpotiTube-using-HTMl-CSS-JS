605f274ae8abd0ba005b7e28cec616e7ff4457c6
Yash Togadiya <yashtogadiya3@gmail.com>
2022-06-10 16:47:17 +0530

    All audio files (25) fetched from google drive.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithRedirect, getRedirectResult, signInWithPopup, signOut } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
+import { getDatabase, set, ref, update } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js";
+import { getAuth, signInWithRedirect, GoogleAuthProvider, signInWithPopup, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,9 +19,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const database = getDatabase(app);
+
 const provider = new GoogleAuthProvider(app);
 
-signin.addEventListener('click', (e) => {
+signingoogle.addEventListener('click', (e) => {
 
   /*  REDIRECTING TO a new page google sign in suthentication */
   // signInWithRedirect(auth, provider);
@@ -66,5 +69,91 @@ signin.addEventListener('click', (e) => {
       const credential = GoogleAuthProvider.credentialFromError(error);
       // ...
     });
+
+
+  login.addEventListener('click', (e) => {
+
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    var username = document.getElementById('username').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+
+        set(ref(database, 'users/' + user.uid), {
+          username: username,
+          email: email
+        })
+
+        alert('user created!');
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        alert(errorMessage);
+        // ..
+      });
+
+  });
+
+  login.addEventListener('click', (e) => {
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+
+        const dt = new Date();
+        update(ref(database, 'users/' + user.uid), {
+          last_login: dt,
+        })
+
+        alert('User loged in!');
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+
+        alert(errorMessage);
+      });
+
+  });
+
+  const user = auth.currentUser;
+  // onAuthStateChanged(auth, (user) => {
+  //   if (user) {
+  //     // User is signed in, see docs for a list of available properties
+  //     // https://firebase.google.com/docs/reference/js/firebase.User
+  //     const uid = user.uid;
+  //     //bla bla bla
+  //     // ...
+  //   } else {
+  //     // User is signed out
+  //     // ...
+  //     //bla bla bla
+  //   }
+  // });
+
+  // // logout.addEventListener('click',(e)=>{
+
+  // //    signOut(auth).then(() => {
+  // //      // Sign-out successful.
+  // //      alert('user loged out');
+  // //    }).catch((error) => {
+  // //      // An error happened.
+  // //      const errorCode = error.code;
+  // //      const errorMessage = error.message;
+
+  // //         alert(errorMessage);
+  // //    });
+
+  // // });
 
 });
--- src/audio/audiodetails.js
+++ src/audio/audiodetails.js
@@ -1,43 +1,211 @@
 const audioDetails = [
     {
-        // img: "./src/audio/Pasoori.jpeg",
-        img: "https://docs.google.com/uc?export=download&id=1wnFWq5-NGdLJZ7v_uRn5BQLjog54VWiv",
-        // src: "./src/audio/Pasoori.mpeg",
-        src: "https://docs.google.com/uc?export=download&id=1-kQ6lbYzk_oinqIe4hgL3UUx-Bz4Gcev",
-        title: "Pasoori - coke studio",
-        artist: "ali shethi , shae gill"
+        // https://drive.google.com/file/d/1CXSljSItoAggXUXhoVCba8Gcercuypua/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1CXSljSItoAggXUXhoVCba8Gcercuypua",
+        // https://drive.google.com/file/d/1iD3Sq2m1tWcBMSREx6dRw0-fc5kr0E4y/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1iD3Sq2m1tWcBMSREx6dRw0-fc5kr0E4y",
+        title: "Bhool Bhulaiyaa 2 - Title Track",
+        artist: "Pritam Chakraborty, Tanishk Bagchi"
     },
     {
-        // img: "./src/audio/Bhool Bhulaiyaa 2 Title Track.jpeg",
-        img: "https://docs.google.com/uc?export=download&id=1xbpm2-mM_J0TaqNWThA7jStuuSHCt-Jr",
-        // src: "./src/audio/Bhool Bhulaiyaa 2 Tilte Track.mpeg",
-        src: "https://docs.google.com/uc?export=download&id=1Nqt5ncGH30g4Tnyjf2FTKHnlC5MyGfRN",
-        title: "Bhool Bhulaiya 2 Title track",
-        artist: "Neeraj Shridhar"
+        // https://drive.google.com/file/d/1eRTPuuqvdKOyYuCaGWV3YOEVcGMVtgHP/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1eRTPuuqvdKOyYuCaGWV3YOEVcGMVtgHP",
+        // https://drive.google.com/file/d/1ToCuJLUFzqtNHcJkZPmwf4OCyWGBBwO-/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1ToCuJLUFzqtNHcJkZPmwf4OCyWGBBwO-",
+        title: "Designer",
+        artist: "Yo Yo Honey Singh, Guru Randhawa"
     },
     {
-        // img: "./src/audio/Makhna.jpeg",
-        img: "https://docs.google.com/uc?export=download&id=1FGFtFkLdEeanZKp7BXuckEhl4lTWqliw",
-        // src: "./src/audio/Makhna.mpeg",
-        src: "https://docs.google.com/uc?export=download&id=1sUvTtLaPJyAR_3IeV8hWemTwEROpSvAa",
+        // https://drive.google.com/file/d/1Fppf1eIbttQMw40rjTATPuyeHcrjFpqL/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1Fppf1eIbttQMw40rjTATPuyeHcrjFpqL",
+        // https://drive.google.com/file/d/1grMLMG8khcdaSw28u7R98rFjoty5WVRX/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1grMLMG8khcdaSw28u7R98rFjoty5WVRX",
+        title: "Dil Ibaadat",
+        artist: "KK"
+    },
+    {
+        // https://drive.google.com/file/d/1pBELc2mmN1buu_SC_x0DOuN1Q1A-TkMR/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1pBELc2mmN1buu_SC_x0DOuN1Q1A-TkMR",
+        // https://drive.google.com/file/d/1bMW3klkm_d-nHJeBfwrZ85KXpF8Zht75/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1bMW3klkm_d-nHJeBfwrZ85KXpF8Zht75",
+        title: "Dil Ki Gali",
+        artist: "Shekhar Ravjiani, Katyayani"
+    },
+    {
+        // https://drive.google.com/file/d/1WYguKHIy3_vjT41r_Ov4mLRfk10eq3qU/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1WYguKHIy3_vjT41r_Ov4mLRfk10eq3qU",
+        // https://drive.google.com/file/d/1zjkWJhrDxjHEm_OWUj9ZtIeDDLNj3Edi/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1zjkWJhrDxjHEm_OWUj9ZtIeDDLNj3Edi",
+        title: "Falak Tu Garaj Tu",
+        artist: "Ravi Basrur, Suchetha Basrur"
+    },
+    {
+        // https://drive.google.com/file/d/1oapBykui5ivjB2Z0RzIUtisjVEWH8esZ/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1oapBykui5ivjB2Z0RzIUtisjVEWH8esZ",
+        // https://drive.google.com/file/d/1kPrXmM_ZcH08UpF5d3qJ03YWhZ6juF4T/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1kPrXmM_ZcH08UpF5d3qJ03YWhZ6juF4T",
+        title: "Jalwanuma",
+        artist: "Javed Ali, Pooja Tiwari"
+    },
+    {
+        // https://drive.google.com/file/d/1f8lUl3E90ZMfBP5LuP6alTLkqPnlY_w-/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1f8lUl3E90ZMfBP5LuP6alTLkqPnlY_w-",
+        // https://drive.google.com/file/d/1K45oAvYtA4SLX_GyKnQN8XHQjjhQY_iS/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1K45oAvYtA4SLX_GyKnQN8XHQjjhQY_iS",
+        title: "Jordaar",
+        artist: "Kapil Jangir"
+    },
+    {
+        // https://drive.google.com/file/d/14lcBJcVwOFKa84RCg4O1NWpkrpuKSrgb/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=14lcBJcVwOFKa84RCg4O1NWpkrpuKSrgb",
+        // https://drive.google.com/file/d/1gvw6NWW0AE79uagtUdUH69oQuboLcTg8/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1gvw6NWW0AE79uagtUdUH69oQuboLcTg8",
+        title: "Jug Jug Jeeve",
+        artist: "Sachin–Jigar, Sachet–Parampara, Sachet Tandon"
+    },
+    {
+        // https://drive.google.com/file/d/19nl2bcO65wEsyTD-qJ5liUO-HAvzsPM2/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=19nl2bcO65wEsyTD-qJ5liUO-HAvzsPM2",
+        // https://drive.google.com/file/d/1eyZb7APbyO-x7CAlC7sqLbOnAKRK8RWX/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1eyZb7APbyO-x7CAlC7sqLbOnAKRK8RWX",
+        title: "Kachiyaan Kachiyaan",
+        artist: "Jubin Nautiyal, Meet Bros"
+    },
+    {
+        // https://drive.google.com/file/d/1tPguKKAiJnAoPC5hRlg9hHBb5yHAFxdw/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1tPguKKAiJnAoPC5hRlg9hHBb5yHAFxdw",
+        // https://drive.google.com/file/d/1pn3po8eDHgomFvqDeKiUTSGHGeR4cIY5/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1pn3po8eDHgomFvqDeKiUTSGHGeR4cIY5",
+        title: "Lut Gaye",
+        artist: "Jubin Nautiyal"
+    },
+    {
+        // https://drive.google.com/file/d/11kkAlwtwBf_fFUe2D9nD98Qy1Z9ol05u/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=11kkAlwtwBf_fFUe2D9nD98Qy1Z9ol05u",
+        // https://drive.google.com/file/d/13dIsDgYVNSERwl8QceQPfVNGKFNJJJUI/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=13dIsDgYVNSERwl8QceQPfVNGKFNJJJUI",
+        title: "Main Royaan",
+        artist: "Yasser Desai, Tanveer Evan, Rajat Nagpal"
+    },
+    {
+        // https://drive.google.com/file/d/1YEUFPo7GfrXfLn31PMRX29EmCn0uH6A5/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1YEUFPo7GfrXfLn31PMRX29EmCn0uH6A5",
+        //https://drive.google.com/file/d/1YRKKWKoOkarXfsVNCj8BGe7RVFH71QPa/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1YRKKWKoOkarXfsVNCj8BGe7RVFH71QPa",
+        title: "Makhmali",
+        artist: "Arijit Singh, Shreya Ghoshal, Shankar-Ehsaan-Loy, Varun Grover"
+    },
+    {
+        // https://drive.google.com/file/d/1Uj-63FzUd0IrnOJPNIcEEyn38gWFHV5G/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1Uj-63FzUd0IrnOJPNIcEEyn38gWFHV5G",
+        // https://drive.google.com/file/d/1EO3x-i5ysXdFuMZ-xiCsmJWjFSWTVw54/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1EO3x-i5ysXdFuMZ-xiCsmJWjFSWTVw54",
         title: "Makhna",
-        artist: "Yaseer Desai , Tanishk bagchi, Asees Kaur"
+        artist: "Yasser Desai, Asees Kaur, Tanishk Bagchi"
     },
     {
-        // img: "./src/audio/Naina da kya kasoor.jpeg",
-        img: "https://docs.google.com/uc?export=download&id=1d183VU5MiGgtE5i-vbogEvKd8iaIWOGt",
-        // src: "./src/audio/Naina da kya kasoor.mpeg",
-        src: "https://docs.google.com/uc?export=download&id=1l0A7xpJID1pZ84idbegTwlrOvS21qOcV",
+        // https://drive.google.com/file/d/1eUHychSYa55q0ysIP8DPGWwRMwVwBDNb/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1eUHychSYa55q0ysIP8DPGWwRMwVwBDNb",
+        // https://drive.google.com/file/d/1ohwECeEo9DialIcrMtAGaZzLy98xxCCO/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1ohwECeEo9DialIcrMtAGaZzLy98xxCCO",
+        title: "Mehabooba",
+        artist: "Ananya Bhat, Ravi Basrur"
+    },
+    {
+        // https://drive.google.com/file/d/1CRtwvkwQlfsnTbgtf-q9nF5rlKyOtueG/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1CRtwvkwQlfsnTbgtf-q9nF5rlKyOtueG",
+        // https://drive.google.com/file/d/11RlvpCF4m85Y2CAOn0QWScz5ObxZH_4C/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=11RlvpCF4m85Y2CAOn0QWScz5ObxZH_4C",
         title: "Naina Da Kya Kasoor",
-        artist: "Ayushman Khuranaa"
+        artist: "Ayushman Khurana"
     },
     {
-        // img: "./src/audio/Senorita.jpeg",
-        img: "https://docs.google.com/uc?export=download&id=1stuBTdBBLiEVXXD1m6llVa9GkgrlDpm_",
-        // src: "./src/audio/Senorita.mpeg",
-        src: "https://docs.google.com/uc?export=download&id=18dDbjlTqa5AvWH_txvy4dkvWNzXJEBZ1",
-        title: "Senorita Zindgi na milegi dubaraa",
-        artist: "María del Mar Fernández"
+        // https://drive.google.com/file/d/1WFNN03A649qWJezxbM4wR0KOaiFAby7g/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1WFNN03A649qWJezxbM4wR0KOaiFAby7g",
+        // https://drive.google.com/file/d/1agyJghE1Rl1X4QG4xnHM49LvdJxNfGIQ/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1agyJghE1Rl1X4QG4xnHM49LvdJxNfGIQ",
+        title: "Nikamma",
+        artist: "Abhimanyu Dasani, Shirley Setia, Samir Soni, Mukesh Agrohari"
+    },
+    {
+        // https://drive.google.com/file/d/1uQpwKTxKlrB4W58LPoHzyCopi1I_Y8f4/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1uQpwKTxKlrB4W58LPoHzyCopi1I_Y8f4",
+        // https://drive.google.com/file/d/1eMrJ1kYJYH1My8coAMHLZKuTXu9Vs2Cc/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1eMrJ1kYJYH1My8coAMHLZKuTXu9Vs2Cc",
+        title: "Pasoori",
+        artist: "Ali Sethy, Shae Gill"
+    },
+    {
+        // https://drive.google.com/file/d/1IEn92TGkVJ7-b7EFmt89AQMSb4WRKOyH/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1IEn92TGkVJ7-b7EFmt89AQMSb4WRKOyH",
+        // https://drive.google.com/file/d/1avO0iIgeMdiqh3ObpT33VPXhE70r8Ilp/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1avO0iIgeMdiqh3ObpT33VPXhE70r8Ilp",
+        title: "Raantaan Lambiyan",
+        artist: "Jubin Nautiyal, Asees Kaur, Tanishk Bagchi"
+    },
+    {
+        // https://drive.google.com/file/d/1RA5oeL32bf8br6QsTyFJbUapsD5Pvlgs/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1RA5oeL32bf8br6QsTyFJbUapsD5Pvlgs",
+        // https://drive.google.com/file/d/1HeGWCT9CcPPc6nSR_RkxRxFweX44OiLR/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1HeGWCT9CcPPc6nSR_RkxRxFweX44OiLR",
+        title: "Rait Zara Si",
+        artist: "Arijit Singh"
+    },
+    {
+        // https://drive.google.com/file/d/1LNgmrVrLAKnlaNPE0jqhyBTiCetvK2-E/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1LNgmrVrLAKnlaNPE0jqhyBTiCetvK2-E",
+        // https://drive.google.com/file/d/1v76IuVlQTkzLFj65I3bCCl_btzgMw4ke/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1v76IuVlQTkzLFj65I3bCCl_btzgMw4ke",
+        title: "Rangisari",
+        artist: "Kanishk Seth"
+    },
+    {
+        // https://drive.google.com/file/d/1r_Ph4hupW46YcdL_naZzS2lOPeh59J19/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1r_Ph4hupW46YcdL_naZzS2lOPeh59J19",
+        // https://drive.google.com/file/d/1u5xYdRxOEPE4pSx8W33oJvoXbZQpCaDZ/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1u5xYdRxOEPE4pSx8W33oJvoXbZQpCaDZ",
+        title: "Senorita",
+        artist: " María del Mar Fernández"
+    },
+    {
+        // https://drive.google.com/file/d/1agohhRpiJlddtzIRdy9O-1sjRBdKmGio/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1agohhRpiJlddtzIRdy9O-1sjRBdKmGio",
+        // https://drive.google.com/file/d/1nYzQbEP5YFGJsOcWcXw-vSF5qymQv6yV/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1nYzQbEP5YFGJsOcWcXw-vSF5qymQv6yV",
+        title: "The Punjaban Song",
+        artist: "Tanishk Bagchi, Zahrah S Khan, Abrar-Ul-Haq, Gippy Grewal, Romy"
+    },
+    {
+        // https://drive.google.com/file/d/1KtNgTVHpqnbojUlq_fTuFu78tAc9FwW1/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1KtNgTVHpqnbojUlq_fTuFu78tAc9FwW1",
+        // https://drive.google.com/file/d/1MKj0J2rrfpIqoyC5D4imZIQzCneNVISx/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1MKj0J2rrfpIqoyC5D4imZIQzCneNVISx",
+        title: "Toofan",
+        artist: "Ravi Basrur, Sachin Basrur, Deepak Blue, Mohan Krishna"
+    },
+    {
+        // https://drive.google.com/file/d/1aVV-0WdFYi56o4FgNUBvmKrfIJIhZWd-/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=1aVV-0WdFYi56o4FgNUBvmKrfIJIhZWd-",
+        // https://drive.google.com/file/d/1uX5JeNWfh8O-KJ95LSqhDbGyJf6VPwt9/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1uX5JeNWfh8O-KJ95LSqhDbGyJf6VPwt9",
+        title: "Tujhe Sochta Hoon",
+        artist: "Pritam Chakraborty"
+    },
+    {
+        // https://drive.google.com/file/d/14p0gcbiQQjDanlyYIHtw1-bCMquKCAPg/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=14p0gcbiQQjDanlyYIHtw1-bCMquKCAPg",
+        // https://drive.google.com/file/d/1L9Wf9Itti8Qc_tLM6N7UWK4Le8fzc8PI/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1L9Wf9Itti8Qc_tLM6N7UWK4Le8fzc8PI",
+        title: "Wallah Wallah",
+        artist: "Ishaan Khan"
+    },
+    {
+        // https://drive.google.com/file/d/177SwJKDnxTVVR9CF5FhD-hMrbkpOB8Yw/view?usp=sharing
+        img: "https://docs.google.com/uc?export=download&id=177SwJKDnxTVVR9CF5FhD-hMrbkpOB8Yw",
+        // https://drive.google.com/file/d/1pZBlO1iCYoOcvG9LwJ98OG57q_ljtQhx/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1pZBlO1iCYoOcvG9LwJ98OG57q_ljtQhx",
+        title: "Yoddha",
+        artist: "Sunidhi Chauhan"
     }
 ];
 
@@ -54,13 +222,15 @@ function miniPlayer(id) {
             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
         </svg>
     `;
-    document.getElementById("miniPlayer").appendChild(miniPlayer)
+    // document.getElementById("miniPlayer").appendChild(miniPlayer)
 }
 
-function playAudio(id) {
+function playAudio() {
     document.getElementById("miniPlayer").innerHTML = null;
     document.getElementById("videoPlayer").innerHTML = null;
-    miniPlayer(id);
+
+    let randonVar = Math.floor(Math.random() * 26);
+    miniPlayer(randonVar);
 }
 
 function closeMiniPlayer() {
