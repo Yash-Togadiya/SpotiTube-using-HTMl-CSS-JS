3a24dfb5fde688b0b58111189a415ff88f3eac12
Yash Togadiya <yashtogadiya3@gmail.com>
2022-06-14 11:51:57 +0530

    Added more audios and videos to website.

--- src/video/videoDetails.js
+++ src/video/videoDetails.js
@@ -42,40 +42,40 @@ const videoDetails = [
     {
         // https://drive.google.com/file/d/1e0fn5h0yvtoGwRtPgrbfdtPk5MlHWrzK/view?usp=sharing
         img: "https://docs.google.com/uc?export=download&id=1e0fn5h0yvtoGwRtPgrbfdtPk5MlHWrzK",
-        // https://drive.google.com/file/d/1FUhOn5S779A-44fBX6daZWaYjKqYuv3E/view?usp=sharing
-        src: "https://docs.google.com/uc?export=download&id=1FUhOn5S779A-44fBX6daZWaYjKqYuv3E",
+        // https://drive.google.com/file/d/1PB4Rkfp0AMBXj0cmlOIHFNDoEIfZvX4m/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1PB4Rkfp0AMBXj0cmlOIHFNDoEIfZvX4m",
         title: "Kusu Kusu",
         artist: "Zahrah S Khan, Dev Negi"
     },
     {
         // https://drive.google.com/file/d/1Kq1zmZv15NfLc651qnpVEwDktZ2l406h/view?usp=sharing
         img: "https://docs.google.com/uc?export=download&id=1Kq1zmZv15NfLc651qnpVEwDktZ2l406h",
-        // https://drive.google.com/file/d/1xkFuKEylJsLl_QWjJ5enT5pVQUrWypJr/view?usp=sharing
-        src: "https://docs.google.com/uc?export=download&id=1xkFuKEylJsLl_QWjJ5enT5pVQUrWypJr",
+        // https://drive.google.com/file/d/1IAPWnNJt4YSXSFh7IWskfmFP27MkqqrE/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1IAPWnNJt4YSXSFh7IWskfmFP27MkqqrE",
         title: "Blue Eyes",
         artist: "Yo Yo Honey Singh"
     },
     {
         // https://drive.google.com/file/d/1JwtHAQvSYR4kjrS1J9fGWsMZUMh9oCQC/view?usp=sharing
         img: "https://docs.google.com/uc?export=download&id=1JwtHAQvSYR4kjrS1J9fGWsMZUMh9oCQC",
-        // https://drive.google.com/file/d/1bfjzEpbRyF5IYD8S-1ItUpJLDHzdBoEn/view?usp=sharing
-        src: "https://docs.google.com/uc?export=download&id=1bfjzEpbRyF5IYD8S-1ItUpJLDHzdBoEn",
+        // https://drive.google.com/file/d/1Sqbkq7QGch8CZ5dfMrj6ZLuJOr32jEfs/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1Sqbkq7QGch8CZ5dfMrj6ZLuJOr32jEfs",
         title: "Baarish Ban Jaana",
         artist: "Stebin Ben, Payal Dev"
     },
     {
         // https://drive.google.com/file/d/1lZwMH70STny9KkWYMgAuxIo7_h-6uhCg/view?usp=sharing
         img: "https://docs.google.com/uc?export=download&id=1lZwMH70STny9KkWYMgAuxIo7_h-6uhCg",
-        // https://drive.google.com/file/d/17UFCPwLxeEJL4piuthOv2rPYKF0b4XQN/view?usp=sharing
-        src: "https://docs.google.com/uc?export=download&id=17UFCPwLxeEJL4piuthOv2rPYKF0b4XQN",
+        // https://drive.google.com/file/d/1VKVL43gywC-6dfGmITJjymsS-owVIjKt/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1VKVL43gywC-6dfGmITJjymsS-owVIjKt",
         title: "Badri Ki Dulhania - Title Track",
         artist: "Monali Thakur, Dev Negi, Ikka Singh, Neha Kakkar"
     },
     {
         // https://drive.google.com/file/d/12zHl_fDIckfjn7iwEgMPku5Jf0avvwbx/view?usp=sharing
         img: "https://docs.google.com/uc?export=download&id=12zHl_fDIckfjn7iwEgMPku5Jf0avvwbx",
-        // https://drive.google.com/file/d/12RQyyoTAkX5pHjhwZV0lbp0H6ZroVwX4/view?usp=sharing
-        src: "https://docs.google.com/uc?export=download&id=12RQyyoTAkX5pHjhwZV0lbp0H6ZroVwX4",
+        // https://drive.google.com/file/d/1-FM444eKx10pijjj-tlL7Us6M_8RMCWw/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1-FM444eKx10pijjj-tlL7Us6M_8RMCWw",
         title: "Dheere Dheere Se Meri Zindagi",
         artist: "Yo Yo Honet Singh"
     },
@@ -90,16 +90,16 @@ const videoDetails = [
     {
         // https://drive.google.com/file/d/1byC9Tk53R__ByVJglGvUPA0yR3qojKKl/view?usp=sharing
         img: "https://docs.google.com/uc?export=download&id=1byC9Tk53R__ByVJglGvUPA0yR3qojKKl",
-        // https://drive.google.com/file/d/1KTUDTfSw2cZpkm3fB0HHQDzjW0NTDX3G/view?usp=sharing
-        src: "https://docs.google.com/uc?export=download&id=1KTUDTfSw2cZpkm3fB0HHQDzjW0NTDX3G",
+        // https://drive.google.com/file/d/1B8l16OU81fwyHF3V0_SlnSjHsebkf4x9/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1B8l16OU81fwyHF3V0_SlnSjHsebkf4x9",
         title: "Dil Galti Kar Baitha Hai",
         artist: "Jubin Nautiyal"
     },
     {
         // https://drive.google.com/file/d/1m4MzVdwxl1IeFU0lY4Uk5hcuuVcX0imI/view?usp=sharing
         img: "https://docs.google.com/uc?export=download&id=1m4MzVdwxl1IeFU0lY4Uk5hcuuVcX0imI",
-        // https://drive.google.com/file/d/1lgelU5huz9ADBge4iQyJRivjOlj9hTQk/view?usp=sharing
-        src: "https://docs.google.com/uc?export=download&id=1lgelU5huz9ADBge4iQyJRivjOlj9hTQk",
+        // https://drive.google.com/file/d/1x6qLs7uLd2V1FP4uAxBEjzxrBSjHXAZf/view?usp=sharing
+        src: "https://docs.google.com/uc?export=download&id=1x6qLs7uLd2V1FP4uAxBEjzxrBSjHXAZf",
         title: "Jugnu",
         artist: "Badshah"
     },
@@ -127,54 +127,12 @@ const videoDetails = [
         title: "Khariyat",
         artist: "Arijit Singh"
     },
-    {
-        img: "https://docs.google.com/uc?export=download&id=",
-        src: "https://docs.google.com/uc?export=download&id=",
-        title: "",
-        artist: ""
-    },
-    {
-        img: "https://docs.google.com/uc?export=download&id=",
-        src: "https://docs.google.com/uc?export=download&id=",
-        title: "",
-        artist: ""
-    },
-    {
-        img: "https://docs.google.com/uc?export=download&id=",
-        src: "https://docs.google.com/uc?export=download&id=",
-        title: "",
-        artist: ""
-    },
-    {
-        img: "https://docs.google.com/uc?export=download&id=",
-        src: "https://docs.google.com/uc?export=download&id=",
-        title: "",
-        artist: ""
-    },
-    {
-        img: "https://docs.google.com/uc?export=download&id=",
-        src: "https://docs.google.com/uc?export=download&id=",
-        title: "",
-        artist: ""
-    },
-    {
-        img: "https://docs.google.com/uc?export=download&id=",
-        src: "https://docs.google.com/uc?export=download&id=",
-        title: "",
-        artist: ""
-    },
-    {
-        img: "https://docs.google.com/uc?export=download&id=",
-        src: "https://docs.google.com/uc?export=download&id=",
-        title: "",
-        artist: ""
-    },
-    {
-        img: "https://docs.google.com/uc?export=download&id=",
-        src: "https://docs.google.com/uc?export=download&id=",
-        title: "",
-        artist: ""
-    },
+    // {
+    //     img: "https://docs.google.com/uc?export=download&id=",
+    //     src: "https://docs.google.com/uc?export=download&id=",
+    //     title: "",
+    //     artist: ""
+    // }
 ]
 
 function videoPlayer(id) {
